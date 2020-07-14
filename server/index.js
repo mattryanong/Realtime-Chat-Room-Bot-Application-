1742324f343d9723685b16a71be9cc84cb7567ef
Matthew Ong <e0309145@u.nus.edu>
2020-07-14 19:17:51 +0800

    added git ignore for server and enabled CORS in index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
@@ -13,6 +14,7 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(router);
+app.use(cors);
 
 io.on('connection', (socket) => {
     socket.on('join', ({ name, room }, callback) => {
