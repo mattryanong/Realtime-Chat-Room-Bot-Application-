10143e3bb1566bd1a5f3f5be9be8874f99c803ed
Matthew Ong <e0309145@u.nus.edu>
2020-07-21 14:08:29 +0800

    added material UI and overrode theme: palette and font

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,30 @@ import ChatbotWidget from './components/ChatbotWidget/ChatbotWidget'
 
 import Join from './components/Join/Join'
 import Chat from './components/Chat/Chat'
+import { makeStyles, useTheme } from '@material-ui/core';
 
-const App = () => (
-    <Router>
-        <div>
-            <ChatbotWidget /> 
-            <Switch>
-                <Route path="/join" component={Join} />
-                <Route path="/chat" component={Chat} />
-            </Switch>
-        </div>
+const useStyles = makeStyles({
+    primaryBg: {
+        background:
+            "linear - gradient(180deg, #325380 10.77 %, #0A1D37 99.98 %, #0A1D37 100 %)"
+    }
+});
 
-    </Router>
-);
+const App = () => {
+    const classes = useStyles();
+    const theme = useTheme();
+    return (
+        <Router>
+            <div>
+                <ChatbotWidget />
+                <Switch>
+                    <Route path="/join" component={Join} />
+                    <Route path="/chat" component={Chat} />
+                </Switch>
+            </div>
+
+        </Router>
+    );
+}
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import App from './App'
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#33547F",
+        },
+        secondary: {
+            main: "#39B8BA",
+        },
+        background: {
+            default: "F3F6FA",
+            paper: "FFFFFF"
+        },
+        neutralDark: {
+            main: "393939"
+        },
+        neutralGrey: {
+            main: "#818181"
+        },
+        neutralLight: {
+            main: "#EEEEEF"
+        }
+    },
+    typography: {
+        fontFamily: 'Monstserrat'
+    }
+});
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
+    , document.querySelector('#root')
+);
+
