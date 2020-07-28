3e0f96bb8b3ba4f46f66aa78aadfbc6ebc3adb87
Matthew Ong <e0309145@u.nus.edu>
2020-07-29 02:08:21 +0800

    finalised css layout for the chat widget

--- client/src/components/ChatbotWidget/ChatbotWidget.js
+++ client/src/components/ChatbotWidget/ChatbotWidget.js
@@ -5,7 +5,7 @@ import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 import { useHistory } from 'react-router-dom';
 import { Typography, Button } from '@material-ui/core';
 
-import './ChatbotWidget.css'
+import './ChatbotWidget.css';
 
 
 function ChatbotWidget() {
@@ -13,45 +13,43 @@ function ChatbotWidget() {
     const history = useHistory();
 
     return (
-        <div className="webchat">
-            <Widget
-                initPayload={"Hello!"}
-                socketUrl={"http://localhost:5005"}
-                socketPath={"/socket.io/"}
-                customData={{ "language": "en" }} // arbitrary custom data. Stay minimal as this will be added to the socket
-                title={"Demo Rasa Webchat"}
-                // embedded={true}
-                showMessageDate
-                showFullScreenButton={true}
-                params={{ storage: "session" }}
-                onSocketEvent={{
-                    'connect': () => console.log('Connection Established'),
-                    'disconnect': () => sessionStorage.clear()
-                }}
-                onWidgetEvent={{
-                    'onChatOpen': () => console.log('Chat open'),
-                    // 'onChatHidden': () => console.log('Chat hidden'),
-                    'onChatClose': () => {
-                        console.log('Chat closed');
-                        confirmAlert({
-                            customUI: ({ onClose }) => {
-                                return (
-                                    <div className='custom-ui'>
-                                        <Typography variant="h3">Were you satisfied with your service?</Typography>
-                                        <Button onClick={onClose}>Yes</Button>
-                                        <Button onClick={() => {
-                                            history.push("/join");
-                                            onClose();
-                                        }}
-                                        >Speak to an agent</Button>
-                                    </div>
-                                )
-                            }
-                        });
-                    }
-                }}
-            />
-        </div>
+        <Widget
+            initPayload={"Hello!"}
+            socketUrl={"http://localhost:5005"}
+            socketPath={"/socket.io/"}
+            customData={{ "language": "en" }} // arbitrary custom data. Stay minimal as this will be added to the socket
+            title={"Demo Rasa Webchat"}
+            // embedded={true}
+            showMessageDate
+            showFullScreenButton={true}
+            params={{ storage: "session" }}
+            onSocketEvent={{
+                'connect': () => console.log('Connection Established'),
+                'disconnect': () => sessionStorage.clear()
+            }}
+            onWidgetEvent={{
+                'onChatOpen': () => console.log('Chat open'),
+                // 'onChatHidden': () => console.log('Chat hidden'),
+                'onChatClose': () => {
+                    console.log('Chat closed');
+                    confirmAlert({
+                        customUI: ({ onClose }) => {
+                            return (
+                                <div className='custom-ui'>
+                                    <Typography variant="h3">Were you satisfied with your service?</Typography>
+                                    <Button onClick={onClose}>Yes</Button>
+                                    <Button onClick={() => {
+                                        history.push("/join");
+                                        onClose();
+                                    }}
+                                    >Speak to an agent</Button>
+                                </div>
+                            )
+                        }
+                    });
+                }
+            }}
+        />
     )
 }
 
